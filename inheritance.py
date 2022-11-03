7f89e2e624e2037280a9a76e1bcf785cf4c1b0fc
Nalule-Tryphine-Ruth <113919554+Nalule-Tryphine-Ruth@users.noreply.github.com>
2022-11-03 15:29:55 +0300

    Add files via upload

---
+++ inheritance.py
@@ -0,0 +1,21 @@
+class Student:
+    def __init__(self,regno,name,age):
+           self.regno = regno
+           self.name = name
+           self.age = age
+           
+    def Study(self):
+               return "I am studying"
+    def Play(self):
+               return "I am playing"
+    def Discuss(self):
+              return "I am Discussing"
+    def __str__(self):
+        return f"I am {self.name} my RegNo is {self.regno} and am {self.age} years"
+    
+    
+          
+          
+student1=Student("J22B13/049","Tryphine",20)
+print(student1)
+print(student1.Study())
